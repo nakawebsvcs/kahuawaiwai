3a4a203185d29a45a9ac6148097afc91de10c451
Jessica Nakamura <jessinaka@Jessicas-MacBook-Pro.local>
2025-05-02 14:10:38 -1000

    Fix exposed API key

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getFunctions } from "firebase/functions";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "REMOVED",
-  authDomain: "kahua-waiwai.firebaseapp.com",
-  projectId: "kahua-waiwai",
-  storageBucket: "kahua-waiwai.firebasestorage.app",
-  messagingSenderId: "548021501750",
-  appId: "1:548021501750:web:bc1ea76f36ed08c774fd97",
-  measurementId: "G-LCJNQHY48L",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
